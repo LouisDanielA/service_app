b78161b531e3b12b2cbb62943d44c8460dfff78d
LouisDanielA <frixbomberman@gmail.com>
2024-01-02 20:32:35 +0600

    Day 1 - 2

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,17 @@
 FROM python:3.9-alpine3.16
 
+ENV DOCKER_DNS 8.8.8.8
+
 COPY requirements.txt /temp/requirements.txt
 COPY service /service
 
 WORKDIR /service
 
 EXPOSE 8000
+
+#RUN apk add postgresql-client build-base postgresql-dev
+RUN apk add build-base postgresql-client postgresql-dev
+
 
 RUN pip install -r /temp/requirements.txt
 #RUN django-admin startproject core
--- service/service/settings.py
+++ service/service/settings.py
@@ -72,11 +72,15 @@ WSGI_APPLICATION = 'service.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+import os
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'HOST': os.environ.get('DB_HOST'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
     }
 }
 
