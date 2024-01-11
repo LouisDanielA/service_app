8d7ddc29957cd4afbd87747941f671ab1f7fce72
LouisDanielA <frixbomberman@gmail.com>
2024-01-11 14:17:11 +0600

    Day 2 - 1

--- service/services/models.py
+++ service/services/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+from clients.models import Client
+
+
+# from service.clients.models import Client
+
+
+# from clients.models import Client
+
+class Service(models.Model):
+    name = models.CharField(max_length=50, blank=False, null=True)
+    full_price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Сервис"
+        verbose_name_plural = "Сервисы"
+
+
+class Plan(models.Model):
+    PLAN_TYPES = (
+        ('full', 'full'),
+        ('student', 'student'),
+        ('discount', 'discount'),
+    )
+    plan_type = models.CharField(choices=PLAN_TYPES, max_length=10)
+    discount_percent = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(100), MinValueValidator(0)])
+
+    def __str__(self):
+        return self.plan_type
+
+    class Meta:
+        verbose_name = "План"
+        verbose_name_plural = "Планы"
+
+
+class Subscription(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.PROTECT, related_name='subscriptions')
+    service = models.ForeignKey(Service, on_delete=models.PROTECT, related_name='services')
+    plan = models.ForeignKey(Plan, on_delete=models.PROTECT, related_name='plans')
+
+    def __str__(self):
+        return self.client.user.get_full_name()
+
+    class Meta:
+        verbose_name = "Подписка"
+        verbose_name_plural = "Подписки"
