5d217da3700ba5f06503bccd29890a835a3dc014
LouisDanielA <frixbomberman@gmail.com>
2024-01-19 21:55:36 +0600

    Day 2 - 2

--- service/service/settings.py
+++ service/service/settings.py
@@ -137,3 +137,16 @@ REST_FRAMEWORK = {
         'rest_framework.parsers.JSONParser'
     ]
 }
+
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'console': {'class': 'logging.StreamHandler'},
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        }
+    }
+}
--- service/services/views.py
+++ service/services/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Prefetch
 from django.shortcuts import render
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
@@ -6,8 +7,12 @@ from .serializers import SubscriptionSerializer
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
-    queryset = Subscription.objects.all()
+    # queryset = Subscription.objects.all().prefetch_related('client').prefetch_related('client__user')
+    queryset = Subscription.objects.all().prefetch_related(
+        Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name', 'user__email'))
+    )
     serializer_class = SubscriptionSerializer
+
 
 
 
