cca11b7d332aad43b83ebd3d03bed184596cea59
LouisDanielA <frixbomberman@gmail.com>
2024-01-20 18:12:35 +0600

    Day 4-2

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -15,10 +15,12 @@ class SubscriptionSerializer(serializers.ModelSerializer):
     email = serializers.CharField(source='client.user.email')
     price = serializers.SerializerMethodField()
 
-    def get_price(self, obj):
-        return (obj.service.full_price -
-                (obj.service.full_price *
-                 (obj.plan.discount_percent / 100)))
+    def get_price(self, instance):
+        # print('self', self)
+        return instance.price
+    #     return (obj.service.full_price -
+                # (obj.service.full_price *
+    #              (obj.plan.discount_percent / 100)))
 
     class Meta:
         model = Subscription
--- service/services/views.py
+++ service/services/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Prefetch
+from django.db.models import Prefetch, F, Aggregate, Sum
 from django.shortcuts import render
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
@@ -11,10 +11,18 @@ class SubscriptionView(ReadOnlyModelViewSet):
     queryset = Subscription.objects.all().prefetch_related(
         Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name', 'user__email')),
         'plan',
-        'service'
-    )
+        # 'service'
+    ).annotate(price=F('service__full_price')-
+                     F('service__full_price') * F('plan__discount_percent')/100)
     serializer_class = SubscriptionSerializer
 
+    def list(self, request, *args, **kwargs):
+        response = super().list(request, *args, **kwargs)
+        response_data = {'result': response.data}
+        response_data['full_price'] = self.queryset.aggregate(full_price=Sum('price')).get('full_price')
+        # print('type price ', type(response_data['full_price']))
+        response.data = response_data
+        return response
 
 
 
